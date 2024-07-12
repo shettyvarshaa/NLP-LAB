b87ef3579ad699e0717b592bf7468681b02e8048
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-12 11:29:25 +0530

    Shortened 4th program

--- p4.py
+++ p4.py
@@ -1,40 +1,29 @@
-from nltk.grammar import CFG
-from nltk.parse.chart import TopDownChartParser, BottomUpChartParser
-
-grammar = CFG.fromstring('''
+import nltk
+from IPython.display import display
+# from nltk.parse.chart import TopDownChartParser, BottomUpChartParser
+grammar = nltk.CFG.fromstring("""
     S -> NP VP
     VP -> V NP | V NP PP
     PP -> P NP
-    V -> "saw"
-    NP -> "I" |"rishabh" | "achal" | Det N | Det N PP
-    Det -> "the" | "a" | "an"
-    N -> "man" | "telescope"
-    P -> "with"
-''')
+    V -> "saw" | "ate" | "walked"
+    NP -> "Janice"  | "Bob" | Det N | Det N PP
+    Det -> "a" | "an" | "the" | "my"|"his"
+    N -> "dog" | "cat" | "telescope" | "park"| "Moon"| "terrace"
+    P -> "in" | "on" | "by" | "with"| "from"
+""")
 
 def parse_sentence(sentence):
     tokens = sentence.split()
-    # Top Down Parsing
-    top_down_parser = TopDownChartParser(grammar)
     print("Top Down parsing results: ")
-    top_down_results = list(top_down_parser.parse(tokens))
-    if top_down_results:
-        for tree in top_down_results:
-            print(tree)
-            tree.draw()
-    else:
-        print("No parse tree found using top-down parsing")
-
-    # Bottom Up Parsing
-    bottom_up_parser = BottomUpChartParser(grammar)
+    rd_parser = nltk.TopDownChartParser(grammar)
+    for tree in rd_parser.parse(tokens):
+        tree.pretty_print()
+        tree.draw()
     print("Bottom Up parsing results: ")
-    bottom_up_results = list(bottom_up_parser.parse(tokens))
-    if bottom_up_results:
-        for tree in bottom_up_results:
-            print(tree)
-            tree.draw()
-    else:
-        print("No parse tree found using bottom-up parsing")
+    sr_parser = nltk.BottomUpChartParser(grammar)
+    for tree in sr_parser.parse(tokens):
+        tree.pretty_print()
+        tree.draw()
 
 user_input = input("Enter a sentence to parse: ")
 parse_sentence(user_input)
